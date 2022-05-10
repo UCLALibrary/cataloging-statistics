84818d25e8e79436c7f5d18813b35d310cd7321e
Andy Kohler <akohler@library.ucla.edu>
2022-05-10 13:19:51 -0800

    Fix typo

--- catstats/views.py
+++ catstats/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import CatStatsForm
 
-# Placeholderf for form in SYS-826; will be fleshed out via SYS-827
+# Placeholder for form in SYS-826; will be fleshed out via SYS-827
 def run_report(request):
 	if request.method == 'POST':
 		pass
