ed569e3e93c4d130f993b8dcdd4e7568765e452b
Andy Kohler <akohler@library.ucla.edu>
2022-05-16 15:56:22 -0800

    SYS-827: Add view and data retrieval/filtering (#4)
    * Also includes functionality from SYS-834, as view and data handling were developed together

--- catstats/views.py
+++ catstats/views.py
@@ -1,10 +1,21 @@
+import logging
+from copy import deepcopy
 from django.shortcuts import render
 from .forms import CatStatsForm
+from catstats.scripts import get_catstats_data
+
+logger = logging.getLogger(__name__)
 
 # Placeholder for form in SYS-826; will be fleshed out via SYS-827
 def run_report(request):
 	if request.method == 'POST':
-		pass
+		form = CatStatsForm(request.POST)
+		if form.is_valid():
+			# For convenience
+			filters = deepcopy(form.cleaned_data)
+			logger.info(f'{filters = }')
+			report_data = get_catstats_data.main(filters)
 	else:
 		form = CatStatsForm()
-	return render(request, 'catstats/catstats.html', {'form': form})
+		report_data = {}
+	return render(request, 'catstats/catstats.html', {'form': form, 'report_data': report_data})
