16fdc97703bb9d9ce507f734b50b2768dc6434eb
Andy Kohler <akohler@library.ucla.edu>
2022-06-11 16:17:36 -0800

    SYS-859: Support summary reports

--- catstats/models.py
+++ catstats/models.py
@@ -16,20 +16,35 @@ class BibRecord(models.Model):
 
 
 class Field962(models.Model):
+	# Subfields can be repeatable (R) or non-repeatable (NR).
+	# Repeatable subfields are stored as 'val1, val2...' which is OK for this purpose.
 	bib_record = models.ForeignKey(BibRecord, on_delete=models.CASCADE)
-	cat_center = models.CharField(max_length=20)	# $a
-	cataloger = models.CharField(max_length=20)		# $b
-	yyyymm = models.CharField(max_length=6)			# $c
-	difficulty = models.CharField(max_length=20)	# $d
-	maint_info = models.CharField(max_length=20)	# $g
-	national_info = models.CharField(max_length=20)	# $h
-	naco_info = models.CharField(max_length=20)		# $i
-	saco_info = models.CharField(max_length=20)		# $j
-	project = models.CharField(max_length=20)		# $k
+	cat_center = models.CharField(max_length=20)	# $a (NR)
+	cataloger = models.CharField(max_length=20)		# $b (NR)
+	yyyymm = models.CharField(max_length=6)			# $c (NR)
+	difficulty = models.CharField(max_length=20)	# $d (NR)
+	maint_info = models.CharField(max_length=20)	# $g (NR)
+	# Moved to RepeatableSubfield
+	# national_info = models.CharField(max_length=20)	# $h (R)
+	# naco_info = models.CharField(max_length=20)		# $i (R)
+	# saco_info = models.CharField(max_length=20)		# $j (R)
+	# project = models.CharField(max_length=20)		# $k (R)
 
 	class Meta:
 		indexes = [
 			models.Index(fields=['cat_center']),
 			models.Index(fields=['cataloger']),
 			models.Index(fields=['yyyymm']),
+		]
+
+class RepeatableSubfield(models.Model):
+	field_962 = models.ForeignKey(Field962, on_delete=models.CASCADE)
+	# for repeatable 962 $h, $i, $j, $k
+	subfield_code = models.CharField(max_length=1)
+	subfield_value = models.CharField(max_length=20)
+
+	class Meta:
+		indexes = [
+			models.Index(fields=['subfield_code']),
+			models.Index(fields=['subfield_value']),
 		]
--- catstats/view_utils.py
+++ catstats/view_utils.py
@@ -1,19 +1,61 @@
 # Supporting functions for view(s)
+from collections import Counter
 
-# Convert report data to cross-tab compatible
-def get_crosstab_data(report_data):
-	# Only values which are used
+def get_difficulties(report_code):
+	if report_code == '01':
+		# 'New' difficulty codes
+		difficulties = ['1', '1+', '2', '2+', '3', '3+', '4', '4+']
+	elif report_code in ['04', '05']:
+		# 'Maintenance' difficulty codes
+		difficulties = [
+			'1+loc', '1+rec', '1+rev', '1loc', '1rec', '1rev',
+			'2+loc', '2+rec', '2+rev', '2loc', '2rec', '2rev',
+			'3+loc', '3+rec', '3+rev', '3loc', '3rec', '3rev',
+			'4+loc', '4+rec', '4+rev', '4loc', '4rec', '4rev']
+	else:
+		difficulties = None
+	return difficulties
+
+
+def get_summary_data(list_of_data):
+	# Transform a list of scalar values into a list of lists,
+	# with each being [value, count].
+	# E.g., ['a', 'b', 'a'] becomes [['a', 2], ['b', 1]]
+	counts = Counter(list_of_data)
+	data_rows = sorted([[k,v] for k,v in counts.items()])
+	data_rows.append(get_total_row(data_rows))
+	return data_rows
+
+def get_total_row(data_rows):
+	# Data rows include string as 1st element, so skip that when summing columns.
+	if data_rows != []:
+		# Didn't find a way to exclude column with zip().
+		total_row = ['Totals'] + [ sum(row[i] for row in data_rows) for i in range(1, len(data_rows[0]))]
+	else:
+		total_row = []
+	return total_row
+
+
+def get_crosstab_data(report_data, row_name, col_name):
+	# Generic function returning crosstab (list of lists)
+	# on row_name and col_name, with counts and totals.
 	# Data rows for crosstab
-	formats = sorted(set([row['resource_type'] for row in report_data]))
+	rows = sorted(set([row[row_name] for row in report_data]))
 	# Data columns for crosstab
-	diffs = sorted(set([row['difficulty'] for row in report_data]))
+	cols = sorted(set([row[col_name] for row in report_data]))
+
+	# For passing variable field names into filter below.
+	row_name__exact = row_name + '__exact'
+	col_name__exact = col_name + '__exact'
 
 	data_rows = []
-	for format in formats:
-		data_row = [format]
+	for row in rows:
+		data_row = [row]
 		row_total = 0
-		for diff in diffs:
-			count = report_data.filter(resource_type__exact=format, difficulty__exact=diff).values_list('count', flat=True)
+		for col in cols:
+			count = report_data.filter(
+				**{row_name__exact:row, col_name__exact:col}
+				).values_list('count', flat=True)
 			if len(count) == 0:
 				val = 0
 			else:
@@ -24,14 +66,7 @@ def get_crosstab_data(report_data):
 		data_rows.append(data_row)
 
 	# Data for template is list of lists
-	column_headers = ['Format'] + diffs + ['Total']
-	# Data rows include format as 1st element, so skip that when summing columns.
-	if data_rows != []:
-		# Didn't find a way to exclude column with zip().
-		total_row = ['Totals'] + [ sum(row[i] for row in data_rows) for i in range(1, len(data_rows[0]))]
-	else:
-		total_row = []
-	data_rows.append(total_row)
+	column_headers = [row_name.title()] + cols + ['Total']
+	data_rows.append(get_total_row(data_rows))
 	# Return column_headers separately to make formatting easier in template.
 	return column_headers, data_rows
-
